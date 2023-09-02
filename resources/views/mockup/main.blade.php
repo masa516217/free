b266cc0dc8a959e04a6ab54cc556cda84b145f25
masamune@example.com <ec2-user@ip-172-31-10-154.ap-northeast-1.compute.internal>
2023-09-02 21:10:20 +0000

    2

---
+++ resources/views/mockup/main.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang=ja>
+    <head>
+        <meta charset="utf-8">
+        <title>main</title>
+        <link rel="stylesheet" href="/css/style.css">
+    </head>
+    <body>
+        <header>
+            <div class=logo>
+                <h2 class="freem">freem</h2>
+            </div>
+        </header>
+        <div class="maincont">
+            <div class="menu"></div>
+        <main>
+            <div class="game"></div>
+            <div class="game"></div>
+            <div class="game"></div>
+            <div class="game"></div>
+            <div class="game"></div>
+            <div class="game"></div>
+            <div class="game"></div>
+            <div class="game"></div>
+            <div class="game"></div>
+            <div class="game"></div>
+            <div class="game"></div>
+            <div class="game"></div>
+            <div class="game"></div>
+            <div class="game"></div>
+            <div class="game"></div>
+            <div class="game"></div>
+            <div class="game"></div>
+            <div class="game"></div>
+        </main>
+        </div>
+        <footer></footer>
+    </body>
+    
+</html>
